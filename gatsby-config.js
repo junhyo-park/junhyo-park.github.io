06b26d580ba478547f8f00fb43f6f494aa0425c8
Junhyo Park <junhyo.paark@gmail.com>
2021-02-16 17:27:36 +0900

    connect GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,7 +54,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `G-0MKY84WGYZ`,
       },
     },
     `gatsby-plugin-feed`,
